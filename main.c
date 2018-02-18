d53375e9c863c8fd6b940ccef0e8a75fc44b9b63
Mohammed Nayeem <mohammednayeem45@gmail.com>
2018-02-18 19:22:56 +0530

    main.c: Create n threads for n args and passes arg1 for thread1, arg2 for thread2, ... etc.

--- main.c
+++ main.c
@@ -1,17 +1,39 @@
 #include<stdio.h>
 #include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+#include <stdlib.h> // exit()
+#define MAX 100
 
-void main(int argc,char *argv[])
-{ char buffer[100];
+// To vaoid warning return type is void * ; It is better to pass void * and
+// inside the thread typecast it to proper data-type.
+void *threadfunc(void *argv)
+{
+printf("In thread\n");
+printf("*argv=%s\n",(char *)argv);
+char buffer[MAX];
 
-  if(argc >= 2)
-  {
-  if(strstr(argv[1], "jpg"))
-    sprintf(buffer,"jpegoptim %s",argv[1]);
-  else if(strstr(argv[1], "png"))
-    sprintf(buffer,"optipng %s",argv[1]);
-  }
-  else
-    printf("./a.out <image|file|audio|video>\n");
+ if(strstr((char *)argv, "jpg")) // If arg matches jpg; then go for jpg encoding or jpg compression.
+   sprintf(buffer,"jpegoptim %s",(char *)argv);
+ else if(strstr((char *)argv, "png"))
+   sprintf(buffer,"optipng %s",(char *)argv);
+
   system(buffer);
+  return 0;
+}
+
+
+int main(int argc,char *argv[])
+{
+  pthread_t tid;
+  int i;
+  if(argc <= 2)
+  {
+    printf("./a.out <image|file|audio|video>\n");
+    exit(255);
+   }
+  for(i=0;i<argc;i++)
+    pthread_create(&tid, NULL, threadfunc, argv[i]);
+  pthread_exit(NULL);
+  return 0;
 }
